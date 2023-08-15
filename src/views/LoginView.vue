b311ef3a527838287a9a77131f96c6180870f472
Tiago Borica <tiago.borica@gmail.com>
2023-08-14 18:31:15 -0700

    Removing boilerplate views and creating a raw login view;

---
+++ src/views/LoginView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <label for="email">Email</label>
+  <input id="email" v-model="text">
+
+  <label for="password">Email</label>
+  <input id="password" v-model="password">
+</template>
