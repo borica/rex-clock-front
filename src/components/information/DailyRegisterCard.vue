1b9a53f84e6fe31a0105fabf661586a94213d04a
Tiago Borica <tiago.borica@gmail.com>
2023-12-10 14:48:16 -0300

    - Creating new DailyRegisterCard component to center user daily registers; - Updating Dashboard to show DailyRegisterCard;

---
+++ src/components/information/DailyRegisterCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="block max-w-sm py-6 px-10 border rounded-lg shadow dark:bg-gray-200 dark:border-gray-300">
+    <ol class="relative border-s-4 border-blue-700">
+      <li v-for="row in this.dailyRows" :key="row.time" class="mb-10 ms-6">
+            <span class="absolute flex items-center justify-center w-3 h-3 bg-blue-100 rounded-full -start-2 ring-8 ring-white dark:ring-blue-900 dark:bg-orange-600"></span>
+        <h3 class="flex items-center justify-between mb-1 text-lg font-semibold text-gray-900">{{ row.time }}
+          <span v-if="row.isLatest" class="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">Latest</span>
+        </h3>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineProps({
+    dailyRows: {
+      type: Array,
+      required: true
+    }
+  })
+</script>
+
+<style scoped>
+
+</style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,12 +1,39 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <div>
-    Logged User!
+  <div class="container mx-auto">
+    <div class="grid grid-flow-row-dense grid-cols-12">
+      <div class="block col-start-3 col-span-4 max-w-sm p-6 border border-gray-200 rounded-lg shadow dark:bg-gray-200 dark:border-gray-300">
+      </div>
+      <div class="col-span-4">
+        <DailyRegisterCard :daily-rows="dailyRegisters" />
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+
+import DailyRegisterCard from "@/components/information/DailyRegisterCard.vue";
+
+const dailyRegisters = [
+  {
+    time: '8:00 AM',
+    isLatest: false
+  },
+  {
+    time: '12:00 PM',
+    isLatest: false
+  },
+  {
+    time: '13:00 PM',
+    isLatest: false
+  },
+  {
+    time: '17:00 PM',
+    isLatest: true
+  }
+]
+
+</script>
 
 <style scoped>
 
